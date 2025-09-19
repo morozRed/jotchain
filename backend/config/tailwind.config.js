4883e1e1df035952a41eb588aff2be339d5d8693
morozRed <moroz.grigory@gmail.com>
2025-09-19 13:57:31 +0200

    add platforms

--- backend/config/tailwind.config.js
+++ backend/config/tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
-    './app/components/**/*.{erb,haml,html,slim,rb}'
+    './app/components/**/*.{erb,haml,html,slim,rb}',
+    './config/initializers/heroicon.rb'
   ],
   theme: {
     extend: {
