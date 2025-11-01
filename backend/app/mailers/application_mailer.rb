0c2dc68f46a4de95067283ecd1ad1d1077b3c9e0
morozRed <moroz.grigory@gmail.com>
2025-11-01 18:53:56 +0100

    Update application_mailer.rb

--- backend/app/mailers/application_mailer.rb
+++ backend/app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "noreply@notifications.jotchain.com"
+  default from: "hey@notifications.jotchain.com"
   layout "mailer"
 end
