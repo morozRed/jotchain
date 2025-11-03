dfbbaab1fe8e37ba431ede5b5b7bedd8c874d183
morozRed <moroz.grigory@gmail.com>
2025-11-03 13:34:34 +0100

    Set default values for Google OAuth env vars
    Adds empty string defaults to GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET in OmniAuth initializer to prevent errors if environment variables are missing.

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -2,8 +2,8 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-           ENV.fetch("GOOGLE_CLIENT_ID"),
-           ENV.fetch("GOOGLE_CLIENT_SECRET"),
+           ENV.fetch("GOOGLE_CLIENT_ID", ''),
+           ENV.fetch("GOOGLE_CLIENT_SECRET", ''),
            {
              scope: "email,profile",
              prompt: "select_account",
